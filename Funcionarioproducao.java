2b1368a1aebd4dc067dd9a20a2a396b735bc6438
Mateus Martins <mateusmartinsmpp@gmail.com>
2026-06-01 23:15:51 -0300

    adiciona classe Funcionarioproducao com cálculo de bônus e exibição de dados

---
+++ Funcionarioproducao.java
@@ -0,0 +1,29 @@
+public class Funcionarioproducao extends Colaborador {
+
+    private double valorPorPeca;
+    private int quantidadeProduzida;
+
+    public Funcionarioproducao(int matricula, String nome, double valorPorPeca, int quantidadeProduzida) {
+        super(matricula, nome);
+        this.valorPorPeca = valorPorPeca;
+        this.quantidadeProduzida = quantidadeProduzida;
+    }
+
+    public double calcularBonus() {
+        return valorPorPeca * quantidadeProduzida;
+    }
+
+    @Override
+    public double calcularSalarioFinal() {
+        return SALARIO_BASE + calcularBonus();
+    }
+
+    @Override
+    public void exibirDados() {
+        System.out.println("Nome: " + nome);
+        System.out.println("Matrícula: " + matricula);
+        System.out.println("Salário Fixo: " + SALARIO_BASE);
+        System.out.println("Produtividade: " + calcularBonus());
+        System.out.println("Salário final: " + calcularSalarioFinal());
+    }
+}
